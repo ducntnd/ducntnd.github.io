4564abb32f2b276ab3ef6aadd81f61a055ef58f1
ducntnd <nhatduc.hoanghapaper@gmail.com>
2019-10-22 11:55:31 +0700

    table

---
+++ table/script.js
@@ -0,0 +1,28 @@
+var duc=document.getElementsByTagName("th")[0]
+duc.style.color="red"
+// $( document ).ready(function() {
+// 		let films = [
+// 		{genre: "Animation", title: "Wildfood", duration: "3:47", date: "2014-07-16"},
+// 		{genre: "Film", title: "The Deer", duration: "6:24", date: "2014-02-28"},
+// 		{genre: "Animation", title: "The Ghost", duration: "11:40", date: "2012-04-10"},
+// 		{genre: "Film", title: "Animals", duration: "6:40", date: "2005-12-21"},
+// 		{genre: "Animation", title: "Wagons", duration: "21:40", date: "2007-04-12"},
+// 	];
+// films.forEach(function(film) {
+//     $("tbody").append(`
+//     <tr>
+//         <td>${film['genre']}</td>
+//         <td>${film['title']}</td>
+//         <td>${film['duration']}</td>
+//         <td>${film['date']}</td>
+//     </tr>`)
+// });
+// // var trs=document.getElementsByTagName("tr");
+// // 		for(var i=0;i<trs.length;i++){
+// // 			if(i%2==0){
+// // 				trs[i].style.background="grey";
+// // 			}
+// // 		}
+// var arr=$("td");
+// arr[0].html()="dd"
+// })
