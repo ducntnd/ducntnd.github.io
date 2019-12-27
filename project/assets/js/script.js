b31fd82ead37421b0084d26631c8dd68a00398d8
ducntnd <47972831+ducntnd@users.noreply.github.com>
2019-12-27 18:51:20 +0700

    project

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -1,2 +1,15 @@
 $('#header').load('layout/header.html');
 $('#footer').load('layout/footer.html');
+$('.side-news').load('layout/sidebar.html');
+var i=0;
+$('.show').on('click',function(){
+	if(i==0){
+		$('.disnone').css('display','flex');
+		$('.show').html('Ẩn đi')
+		i++
+	}else{
+		$('.disnone').css('display','none');
+		$('.show').html('Hiện thêm')
+		i--
+	}
+})
